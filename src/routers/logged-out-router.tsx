ebe0b3a81ac6daeac6d4fe9d0c8739d9928dc7b2
ysjkof <77876601+ysjkof@users.noreply.github.com>
2021-10-29 14:59:19 +0900

    설정: react-hook-form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,8 +1,47 @@
+import { useForm } from "react-hook-form";
 import { isLoggedInVar } from "../apollo";
 
-export const LoggedOutRouter = () => (
-  <div>
-    <h1>Logged Out</h1>
-    <button onClick={() => isLoggedInVar(true)}>Click to Login</button>
-  </div>
-);
+// typescript와 react hook form 연결.
+interface IForm {
+  email: string;
+  password: string;
+}
+
+export const LoggedOutRouter = () => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+    handleSubmit,
+  } = useForm<IForm>();
+
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onValid = () => {
+    console.log("Can't create Account");
+  };
+
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onValid)}>
+        <input
+          {...register("email", {
+            required: "This is required",
+            pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+          })}
+        />
+        {errors.email?.message && (
+          <span className="font-bold text-red-600">{errors.email.message}</span>
+        )}
+        {errors.email?.type && (
+          <span className="font-bold text-red-600">Only Gmail allowed</span>
+        )}
+        <input {...register("password")} />
+        <button className="bg-yellow-300 text-white">SUBMIT</button>
+      </form>
+      <button onClick={() => isLoggedInVar(true)}>Click to Login</button>
+    </div>
+  );
+};
