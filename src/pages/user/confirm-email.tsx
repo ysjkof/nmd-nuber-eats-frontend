1e11210a6bc585d78c6ef99f3d5033b8dfaa49ff
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-11-12 10:17:41 +0900

    기타: Helmet

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,5 +1,6 @@
 import { gql, useApolloClient, useMutation } from "@apollo/client";
 import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { useHistory } from "react-router";
 import { useMe } from "../../hooks/useMe";
 import {
@@ -60,6 +61,9 @@ export const ConfirmEmail = () => {
   }, []);
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Verify Email | Nuber Eats</title>
+      </Helmet>
       <h2 className="text-lg mb-1 font-medium">Confirming email...</h2>
       <h4 className="text-gray-700 text-sm">
         Please wait, don't close this page...
