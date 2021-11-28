768922f744ccd11c2a2e8b6b39cff7675462e6bd
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-11-28 12:45:10 +0900

    change port

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,7 @@ module.exports = {
     tagName: "gql",
     service: {
       name: "nmd-nuber-eats-backend",
-      url: "http://localhost:4000/graphql",
+      url: "http://localhost:3333/graphql",
     },
   },
 };
