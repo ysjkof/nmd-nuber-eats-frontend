2b338183369a00af4afcd0110ac0b2232dd0b391
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-03-29 21:39:29 +0900

    22 MAKING AN ORDER; 22.0 Extending the Dish Component

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -1,17 +1,38 @@
+import { myRestaurant_myRestaurant_restaurant_menu_options } from "../__generated__/myRestaurant";
+
 interface IDishProps {
   description: string;
   name: string;
   price: number;
+  isCustomer?: boolean;
+  options?: myRestaurant_myRestaurant_restaurant_menu_options[] | null;
 }
 
-export const Dish: React.FC<IDishProps> = ({ description, name, price }) => {
+export const Dish: React.FC<IDishProps> = ({
+  description,
+  name,
+  price,
+  isCustomer = false,
+  options,
+}) => {
   return (
-    <div className="cursor-pointer border py-4 px-8 transition-all hover:border-gray-800">
+    <div className=" cursor-pointer border px-8 py-4 transition-all hover:border-gray-800 ">
       <div className="mb-5">
         <h3 className="text-lg font-medium ">{name}</h3>
         <h4 className="font-medium">{description}</h4>
       </div>
       <span>${price}</span>
+      {isCustomer && options && options?.length !== 0 && (
+        <div>
+          <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
+          {options?.map((option, index) => (
+            <span className="flex items-center" key={index}>
+              <h6 className="mr-2">{option.name}</h6>
+              <h6 className="text-sm opacity-75">(${option.extra})</h6>
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
